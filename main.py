c222cb7c8fc10588a5363cfc2b23321bd0b8cb7f
Becausnt <github.unmanned547@passmail.com>
2026-08-17 09:00:50 +0200

    Bugfix

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import helper
 from flask import Flask, request, Response, render_template, redirect, url_for
 app = Flask(__name__)
+app.logger.setLevel('INFO')
 
 @app.route("/")
 def index():
     items = helper.get_all()
+    app.logger.info(f"Rendering index with items: {items}")
     return render_template('index.html', items=items)
 
 
 @app.route('/add', methods=["POST"])
 def add():
     text = request.form.get("text")
+    app.logger.info(f"Adding item: {text}")
     helper.add(text)
     return redirect(url_for("index"))
 
